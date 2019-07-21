c4652ec354a43a8f8ab5a54ad3a306756259c2fa
Courtney Fink <courtney55@knights.ucf.edu>
2019-07-21 23:27:20 +0000

    edited placescontroller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,7 +8,19 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
+  def create
+    @place = Place.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def aboutus
   end
   
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
